adec3c8af78fbdbefc52e6884ae05481b444a20f
murinho99 <picpac70@yahoo.com>
2021-11-08 18:25:11 +0200

    Fix delay F_CPU and clean Main

--- DisplayProject/DisplayProject/main.c
+++ DisplayProject/DisplayProject/main.c
@@ -6,7 +6,7 @@
  */ 
 
 #define F_CPU 12000000UL
-#include <util/delay.h>
+#include "delay.h"
 #include "USART.h"
 #include "I2C.h"
 #include "SPI.h"
@@ -14,37 +14,9 @@
 
 int main(void)
 {
-	I2C_Init(10000UL,I2C_Prescaler_64);
-	uint8_t addr=0x50;
-	uint8_t i=0;
-    while (1) 
-    {	
 
-		_delay_ms(100);
+	while(1){
 		
-		I2C_Start();
-		I2C_Write_Slave_Addr(TW_SLA_WRITE_ADDR(addr));
-		I2C_Write(0x00);
-		I2C_Write(0x00);
-		I2C_Write((i++)%255);
-		I2C_Stop();
-		
-		_delay_ms(5);
-		//Set address to 0x0000
-		I2C_Start();
-		I2C_Write_Slave_Addr(TW_SLA_WRITE_ADDR(addr));
-		I2C_Write(0x00);
-		I2C_Write(0x00);
-		
-		//Start I2C comm again, with read address
-		I2C_Start();
-		I2C_Write_Slave_Addr(TW_SLA_READ_ADDR(addr));
-		I2C_Read(false);
-		I2C_Stop();
-
-
-
-	
-    }
+	}
 }
 
