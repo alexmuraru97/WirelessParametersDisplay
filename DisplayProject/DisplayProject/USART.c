600b27dfbf0a452f6321a8aed7f85b44eff79404
murinho99 <picpac70@yahoo.com>
2021-11-08 18:40:34 +0200

    Initial USART_DEV commit

--- DisplayProject/DisplayProject/USART.c
+++ DisplayProject/DisplayProject/USART.c
@@ -5,3 +5,24 @@
  *  Author: Wobberr
  */ 
 #include "USART.h"
+
+void USART_Init(unsigned int baudrate)
+{
+	UBRR0H = (unsigned char)(BAUD_RATE(baudrate) >> 8);
+	UBRR0L = (unsigned char)(BAUD_RATE(baudrate) & 0xFF);
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0); 
+	DDRD |= (1 << DDD1);
+	DDRD &= ~(1 << DDD0);
+}
+
+void USART_Write(unsigned char data)
+{
+	while(!(UCSR0A&(1<<UDRE0))){;}
+	UDR0=data;	
+}
+
+unsigned char USART_Read(void)
+{
+	while(!(UCSR0A&(1<<RXC0))){;}
+	return UDR0;	
+}
--- DisplayProject/DisplayProject/USART.h
+++ DisplayProject/DisplayProject/USART.h
@@ -5,13 +5,19 @@
  *  Author: Wobberr
  */ 
 
-#include <avr/io.h>
+
+#include "Env.h"
 
 #ifndef USART_H_
 #define USART_H_
 
 
+#define BAUD_RATE(BAUD) (F_CPU/16/BAUD - 1)
 
+
+void USART_Init(unsigned int baudrate);
+void USART_Write(unsigned char data);
+unsigned char USART_Read(void);
 
 
 #endif /* USART_H_ */
